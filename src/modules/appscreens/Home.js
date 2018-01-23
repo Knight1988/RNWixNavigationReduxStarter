aed39f0309edbcca7ed1d7ea677c9e0a90b3e05c
Squall Leonhart <Knight1988@gmail.com>
2018-01-23 15:35:52 +0700

    add vector icons

--- src/modules/appscreens/Home.js
+++ src/modules/appscreens/Home.js
@@ -1,17 +1,48 @@
 import React, { PropTypes, Component } from 'react';
 import {
+    Image,
     Text,
     View
 } from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as homeActions from '../../actions/homeActions';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { iconsMap } from './../../app-icons';
 
 class Home extends Component {
+
+    static navigatorButtons = {
+        rightButtons: [
+            {
+                icon: iconsMap['ios-home'], // for icon button, provide the local image asset name
+                id: 'add' // id for this button, given in onNavigatorEvent(event) to help understand which button was clicked
+            },
+            {
+                icon: require('./../../assets/homeicon.png'), // for icon button, provide the local image asset name
+                id: 'Test' // id for this button, given in onNavigatorEvent(event) to help understand which button was clicked
+            },
+        ],
+        leftButtons: [
+            {
+                id: 'sideMenu'
+            }
+        ],
+        animated: false
+    };
+
+    constructor(props){
+        super(props);
+
+        this.props.navigator.setButtons(Home.navigatorButtons);
+    }
+
     render() {
         return(
             <View>
+                <Image source={iconsMap['ios-home']} style={{width: 20, height: 20}} />
                 <Text>Home Page Content added here</Text>
+                <Icon name='ios-home' size={20} />
             </View>
         )
     }
